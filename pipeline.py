93a4e390ac45d6196b62727bcb926ab00460468c
chandani saw <chandanisaw85@gmail.com>
2026-07-10 23:12:43 +0530

    improvement

--- pipeline.py
+++ pipeline.py
@@ -52,7 +52,7 @@ def run_research_pipeline(topic : str) -> dict:
 
     print("\n Final Report\n",state['report'])
 
-    #CRITIC CHAIN - step 4
+    #critic chain
 
     print("\n"+" ="*50)
     print("step 4 - critic is reviewing the report ")
